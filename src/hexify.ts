cd74e396ac42ed7e2353f39ab8b53d4ca10b4b7f
Victor Nascimento Bakke <gipphe@gmail.com>
2022-10-07 10:04:06 +0200

    fix missing alpha values

--- src/hexify.ts
+++ src/hexify.ts
@@ -9,13 +9,13 @@ import {
 
 const convertColors = <A, B>(
   fn: (x: A) => B,
-  c: Record<string, A>,
+  c: Record<string, A>
 ): Record<string, B> =>
   Object.fromEntries(Object.entries(c).map(([prop, val]) => [prop, fn(val)]));
 
 const convertSemanticTokenColor = <A, B>(
   fn: (x: A) => B,
-  c: SemanticTokenColor<A>,
+  c: SemanticTokenColor<A>
 ): SemanticTokenColor<B> => {
   const { foreground: _foreground, ...rest } = c;
   return {
@@ -26,17 +26,18 @@ const convertSemanticTokenColor = <A, B>(
 
 const convertSemanticTokenColors = <A, B>(
   fn: (x: A) => B,
-  c: SemanticTokenColors<A>,
+  c: SemanticTokenColors<A>
 ): SemanticTokenColors<B> =>
   Object.fromEntries(
     Object.entries(c).map(([scope, val]) => [
       scope,
       isSemanticTokenColor(val) ? convertSemanticTokenColor(fn, val) : fn(val),
-    ]),
+    ])
   );
 
 const convertTokenColors =
-  <A, B>(fn: (x: A) => B) => (c: TokenColors<A>): TokenColors<B> => {
+  <A, B>(fn: (x: A) => B) =>
+  (c: TokenColors<A>): TokenColors<B> => {
     if ("name" in c) {
       const { foreground, ...rest } = c.settings;
       return {
@@ -52,13 +53,14 @@ const convertTokenColors =
         Object.entries(c.settings as Record<string, A>).map(([key, v]) => [
           key,
           fn(v),
-        ]),
+        ])
       ),
     };
   };
 
 export const convertTheme =
-  <A, B>(fn: (x: A) => B) => (t: Theme<A>): Theme<B> => ({
+  <A, B>(fn: (x: A) => B) =>
+  (t: Theme<A>): Theme<B> => ({
     name: t.name,
     colors: convertColors(fn, t.colors),
     semanticTokenColors: convertSemanticTokenColors(fn, t.semanticTokenColors),
@@ -68,7 +70,7 @@ export const convertTheme =
       : undefined),
   });
 
-const hexify = (v: Color): string => v.hex();
+const hexify = (v: Color): string => (v.alpha() < 1 ? v.hexa() : v.hex());
 const unhexify = (v: string): Color => Color(v);
 
 export const hexifyTheme = convertTheme(hexify);
